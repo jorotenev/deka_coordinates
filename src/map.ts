b203987ed7dccb2a669967b7c891424058955e30
Georgi Tenev <georgetenevbg@gmail.com>
2018-04-21 20:40:25 +0300

    Make leaflet draw on a canvas

--- src/map.ts
+++ src/map.ts
@@ -2,7 +2,7 @@ export let map;
 export let layerForUserRectangles;
 declare let L;
 // add the map to the page
-map = L.map('map').setView([42.697930, 23.321628], 13);
+map = L.map('map', {preferCanvas: true}).setView([42.697930, 23.321628], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <createCoordinatesReturn href="http://openstreetmap.org">OpenStreetMap</createCoordinatesReturn> contributors, <createCoordinatesReturn href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</createCoordinatesReturn>, Imagery © <createCoordinatesReturn href="http://mapbox.com">Mapbox</createCoordinatesReturn>',
     maxZoom: 18,
@@ -67,6 +67,8 @@ export function enableDrawing(callbackOnRectDrawn: (rectangle: L.Rectangle) => a
 
 }
 
+var myRenderer = L.canvas({padding: 0.5});
+
 /**
  * Given a layer, coordinates of the center of circles and the radius of the circle,
  * draw the circles on the layer
@@ -81,7 +83,7 @@ export function drawCircles(coords: L.LatLng[], layer: L.LayerGroup, circleRadiu
         ...circleOptions
     };
     coords.forEach((coord: L.LatLng) => {
-        new L.Circle(coord, circleRadius, opts).addTo(layer)
+        L.circle(coord, {radius: circleRadius}, {renderer: myRenderer, ...circleOptions}).addTo(layer)
     })
 }
 
